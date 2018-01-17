06e6b92271d3b3d1bd635788358e98eda9927951
Ivan Lenon <ivan.lenon@outlook.com>
2018-01-17 13:17:05 -0200

    navbar

---
+++ navbar/navbar.inc.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>HEADER TITLES</title>
+
+	<!-- Bootstrap and Styles CSS -->
+	<link rel="stylesheet" type="text/css" href="assets/styles/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+	<link rel="stylesheet" href="assets/styles/style.css">
+</head>
+
+<body>
+
+	<header class="header">
+
+		<div class="navbar navbar-default navbar-static-top">
+			<div class="container">
+
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<!-- NAVBAR BRAND <a class="navbar-brand" href="index.php"><img src="assets/images/logo-andrade.png" alt="Andrade"/></a> -->
+				</div>
+
+				<div class="navbar-collapse collapse ">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="index.php">Home</a></li>
+						<li><a href="about.html">Sobre</a></li>
+						<li><a href="services.html">Serviços</a></li>
+						<li><a href="obras.html">Obras</a></li>
+						<li><a href="contact.html">Contato</a></li>
+						<li class="phone-header"><span>19 3434-9797</span></li>
+					</ul>
+				</div>
+
+			</div> <!-- /container -->
+		</div> <!-- /navbar -->
+
+	</header> <!-- /header -->
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+</body>
+</html>
